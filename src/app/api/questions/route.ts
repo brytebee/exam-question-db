0759505bc9cd27d152208104a1cfc6501f05fed3
Bright Atsighi <brytebee@gmail.com>
2024-08-19 14:33:31 +0100

    Update API route and implemented it when submitted

--- src/app/api/questions/route.ts
+++ src/app/api/questions/route.ts
@@ -1,50 +1,55 @@
 import { NextResponse } from "next/server";
-import { getClient } from "@/app/lib/db";
+import prisma from "@/app/lib/prismaClient";
+import { Question } from "@prisma/client";
 
-// GET: Retrieve all questions along with their associated examInfo
 export async function GET() {
-  const client = await getClient();
-  const result = await client.query(`
-    SELECT q.id, q.question, q.options, q.correct_answer, q.image_url, e.exam, e.subject, e.year, e.total_questions
-    FROM questions q
-    JOIN exam_info e ON q.exam_id = e.id
-  `);
-  return NextResponse.json(result.rows);
+  try {
+    const questions = await prisma.question.findMany({
+      include: { exam: true },
+    });
+    return NextResponse.json(questions);
+  } catch (error) {
+    return NextResponse.error();
+  }
 }
 
-// POST: Insert a new question with its associated examInfo
 export async function POST(request: Request) {
-  const { question, options, correctAnswer, imageUrl, examInfo } =
-    await request.json();
-  const { exam, subject, year, totalQuestions } = examInfo;
+  const { questions, examInfo } = await request.json();
 
-  const client = await getClient();
+  try {
+    let exam = await prisma.examInfo.findUnique({
+      where: {
+        exam_subject_year: {
+          exam: examInfo.exam,
+          subject: examInfo.subject,
+          year: examInfo.year,
+        },
+      },
+    });
 
-  // Check if the examInfo already exists
-  const examResult = await client.query(
-    "SELECT id FROM exam_info WHERE exam = $1 AND subject = $2 AND year = $3",
-    [exam, subject, year]
-  );
+    if (!exam) {
+      exam = await prisma.examInfo.create({
+        data: {
+          exam: examInfo.exam,
+          subject: examInfo.subject,
+          year: examInfo.year,
+          totalQuestions: examInfo.totalQuestions,
+        },
+      });
+    }
 
-  let examId: number;
+    await prisma.question.createMany({
+      data: questions.map((question: Question) => ({
+        question: question.question,
+        options: question.options,
+        correctAnswer: question.correctAnswer,
+        imageUrl: question.imageUrl,
+        examId: exam.id,
+      })),
+    });
 
-  if (examResult.rows.length > 0) {
-    // If examInfo exists, use the existing id
-    examId = examResult.rows[0].id;
-  } else {
-    // If examInfo does not exist, insert it and get the new id
-    const insertExamResult = await client.query(
-      "INSERT INTO exam_info (exam, subject, year, total_questions) VALUES ($1, $2, $3, $4) RETURNING id",
-      [exam, subject, year, totalQuestions]
-    );
-    examId = insertExamResult.rows[0].id;
+    return NextResponse.json({ message: "Questions added successfully" });
+  } catch (error) {
+    return NextResponse.error();
   }
-
-  // Insert the question with the associated exam_id
-  await client.query(
-    "INSERT INTO questions (question, options, correct_answer, image_url, exam_id) VALUES ($1, $2, $3, $4, $5)",
-    [question, options, correctAnswer, imageUrl, examId]
-  );
-
-  return NextResponse.json({ message: "Question added successfully" });
 }
