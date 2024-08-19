44a0dea91dc0d3ca672e3f25536f4b859c88f334
Bright Atsighi <brytebee@gmail.com>
2024-08-19 14:32:53 +0100

    Removed unused sql codes, added prisma client

---
+++ src/app/lib/prismaClient.ts
@@ -0,0 +1,7 @@
+import { PrismaClient } from "@prisma/client";
+
+// Create a single instance of PrismaClient
+const prisma = new PrismaClient();
+
+// Export the single instance
+export default prisma;
