f4f1d7ba24cdb4303ed33395b4b3e81a1d1bace7
Bright Atsighi <brytebee@gmail.com>
2024-08-21 00:10:48 +0100

    Added middleware to protect pages

---
+++ src/middleware.ts
@@ -0,0 +1,29 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+import { getToken } from "next-auth/jwt";
+
+export async function middleware(req: NextRequest) {
+  const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
+
+  const protectedRoutes = [
+    "/exam/add-exam",
+    "/exam/add-questions",
+    "/exam/preview",
+    "/exam/view-questions",
+  ];
+
+  if (protectedRoutes.some((route) => req.nextUrl.pathname.startsWith(route))) {
+    if (!token) {
+      const url = req.nextUrl.clone();
+      url.pathname = "/auth/signin";
+      url.searchParams.set("callbackUrl", req.nextUrl.pathname);
+      return NextResponse.redirect(url);
+    }
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: ["/exam/:path*"],
+};
